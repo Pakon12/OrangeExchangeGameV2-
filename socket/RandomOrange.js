8c5f37bf9e52b9dd1efa6b7295ad8d3eed2665c6
Pakon12 <okpkmyang12@gmail.com>
2024-09-26 04:19:19 +0700

    update getRandomOrange

--- socket/RandomOrange.js
+++ socket/RandomOrange.js
@@ -3,13 +3,9 @@ const oranges = ['orangeA', 'orangeB', 'orangeC', 'orangeD', 'orangeE', 'orangeF
 function getRandomOrange(number) {
     switch (number) {
         case 1:
-            return { orangeP1: oranges[1], orangeP2: oranges[1] ,showOrange: 2 ,position: null};
+            return { orangeP1: oranges[3], orangeP2: oranges[2] ,showOrange: 2 ,position: null};
         case 2:
-            return { orangeP1: oranges[0], orangeP2: oranges[0] ,showOrange: 1 ,position: "P1"};
-        // case 1:
-        //     return { orangeP1: oranges[3], orangeP2: oranges[2] ,showOrange: 2 ,position: null};
-        // case 2:
-        //     return { orangeP1: oranges[5], orangeP2: oranges[4] ,showOrange: 1 ,position: "P1"};
+            return { orangeP1: oranges[5], orangeP2: oranges[4] ,showOrange: 2 ,position: null};
         case 3:
             return { orangeP1: oranges[1], orangeP2: oranges[0] ,showOrange: 2 ,position: null};
         case 4:
